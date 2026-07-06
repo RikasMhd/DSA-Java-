dbff4a26f4da4f43e24af73ed7d8416caf1237d6
Rikas <rrikas341@gmail.com>
2026-07-06 22:24:45 +0530

    Enhance BubbleSort with user input and method extraction
    Refactored BubbleSort to include user input and a separate bubbleSort method.

--- Array (Labsheet-3)/Questions/BubbleSort.java
+++ Array (Labsheet-3)/Questions/BubbleSort.java
@@ -1,21 +1,54 @@
+import java.util.Scanner;
 public class BubbleSort {
-    public static void main(String[] args) {
-        int[] arr = {64, 34, 25, 12, 22, 11, 90};
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // Swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swapped = true;
                 }
             }
+            // if no swaps occurred, array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        System.out.print("Sorted array: ");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
+        System.out.print("Original array: ");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.print("Sorted array (Bubble Sort): ");
+        printArray(arr);
+
+        sc.close();
     }
 }
