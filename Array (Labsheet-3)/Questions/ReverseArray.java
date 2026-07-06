27a66d70cb892dd341b856ab8c37924e299ba8a5
Rikas <rrikas341@gmail.com>
2026-07-06 22:25:54 +0530

    Update ReverseArray.java

--- Array (Labsheet-3)/Questions/ReverseArray.java
+++ Array (Labsheet-3)/Questions/ReverseArray.java
@@ -1,19 +1,46 @@
+import java.util.Scanner;
 public class ReverseArray {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int left = 0, right = arr.length - 1;
 
-        while (left < right) {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
+    public static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Original array: ");
+        printArray(arr);
+
+        reverse(arr);
 
         System.out.print("Reversed array: ");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
+        printArray(arr);
+
+        sc.close();
     }
 }
