b2856d43044aeb2d56ba155a84c0b105bf721be0
Rikas <rrikas341@gmail.com>
2026-07-06 22:26:08 +0530

    Update SelectionSort.java

--- Array (Labsheet-3)/Questions/SelectionSort.java
+++ Array (Labsheet-3)/Questions/SelectionSort.java
@@ -1,25 +1,51 @@
+import java.util.Scanner;
 public class SelectionSort {
-    public static void main(String[] args) {
-        int[] arr = {64, 34, 25, 12, 22, 11, 90};
 
-        for (int i = 0; i < arr.length - 1; i++) {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
-
-            for (int j = i + 1; j < arr.length; j++) {
+            for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
 
-            // Swap minimum with current position
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
+    public static void printArray(int[] arr) {
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        System.out.print("Sorted array: ");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
+        System.out.print("Original array: ");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.print("Sorted array (Selection Sort): ");
+        printArray(arr);
+
+        sc.close();
     }
 }
