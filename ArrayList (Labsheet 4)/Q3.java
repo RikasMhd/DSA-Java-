4dcc88be721fbfdac93354ee6597678b070664b2
Rikas <rrikas341@gmail.com>
2026-07-16 21:54:23 +0530

    Update Q3.java

--- ArrayList (Labsheet 4)/Q3.java
+++ ArrayList (Labsheet 4)/Q3.java
@@ -74,3 +74,17 @@ public class Q3{
         traverse(studentList);
     }
 }
+
+
+/*
+Let's say you have a custom class called Student that represents student information. It has the following attributes: name (String), age (int), and grade (double).
+Your task is to create an ArrayList named studentList and perform the following operations:
+i. Add three student objects to the studentList with the following details:
+a. Student 1: Name - "John Doe", Age - 18, Grade - 85.5
+b. Student 2: Name - "Jane Smith", Age - 17, Grade - 92.0
+c. Student 3: Name - "Michael Johnson", Age - 19, Grade - 78.3
+ii. Print the details of all the students in the studentList.
+iii. Update the grade of "John Doe" to 90.2.
+iv. Remove "Jane Smith" from the studentList.
+v. Print the details of all the students in the studentList after the modifications.
+    */
